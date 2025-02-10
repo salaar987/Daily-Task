3c06376006c524f17ad846a1c2ab395c18387f3c
salaar987 <154842278+salaar987@users.noreply.github.com>
2025-02-10 15:36:57 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+// to use form  parsers for form
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));  // for static files
+app.set('view engine','ejs'); // rendering ejs pages
+
+// handling the task showing part
+app.get("/",function(req,res){
+    fs.readdir(`./files`,function(err,files){
+        res.render("index",{files: files});
+    })
+});
+app.get("/file/:filename",function(req,res){
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
+        res.render('show',{filename: req.params.filename,filedata:filedata});
+    })
+});
+app.get('/edit/:filename',function(req,res){
+    res.render('edit',{filename: req.params.filename});
+})
+app.post('/edit',function(req,res){
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,function(err){
+        res.redirect("/")
+    })
+})
+app.post("/create",function(req,res){
+        fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+            res.redirect("/");
+        });
+
+    });
+
+app.get("/profile/:username",function(req,res){    // dynamic routing
+    // req.params.username
+    res.send(`welcome,${req.params.username}`);
+
+});
+app.get("/author/:username/:age",function(req,res){    // dynamic routing
+    // req.params.username
+    res.send(`welcome,${req.params.username} of age ${req.params.age}`);
+});
+
+
+app.listen(3000,function(){
+    console.log("it is runn");
+})
